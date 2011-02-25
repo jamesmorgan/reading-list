16c650714662dbfea9524ea108bff2254e3a0fdf
James Morgan <james.morgan@leadx.com>
2011-02-25 11:45:39 +0000

    Inital Reading List Titles

--- ReadingList.java
+++ ReadingList.java
@@ -4,7 +4,109 @@
 public class ReadingList {
 
 	public static void main(final String[] args) {
-
+		System.out.println("My Reading List");
 	}
+	
+	/* Books Read */
+	
+	/**
+	 * <h1>Head First Design Patterns<h1>
+	 * 
+	 */
 
+	/**
+	 * <h1>Advanced Flex 3</h1>
+	 * 
+	 */
+
+	/**
+	 * <h1>Clean Code: A Handbook of Agile Software Craftsmanship</h1>
+	 * 
+	 */
+
+	/**
+	 * <h1>Agile Software Development, Principles, Patterns, and Practices</h1>
+	 * 
+	 */
+
+	/**
+	 * <h1>Effective Java (2nd Eddition)</h1>
+	 * 
+	 */
+
+	/**
+	 * <h1>97 Things Ever programmer should know</h1>
+	 * 
+	 */
+
+	/**
+	 * <h1>JavaScript: The Good Parts</h1>
+	 * 
+	 */
+
+	/**
+	 * <h1>Kent Beck - Implementation Patterns</h1>
+	 * 
+	 */
+
+	/* Books Reading */
+	
+	/**
+	 * 
+	 */
+	
+	/**
+	 * 
+	 */
+	
+	/* Books To Reading */
+
+	/**
+	 * 
+	 */	
+
+	/**
+	 * 
+	 */	
+	
+	/**
+	 * 
+	 */	
+	
+	/**
+	 * 
+	 */	
+	
+	/**
+	 * 
+	 */	
+	
+	/**
+	 * 
+	 */	
+	
+	/**
+	 * 
+	 */	
+	
+	/**
+	 * 
+	 */	
+	
+	/**
+	 * 
+	 */	
+	
+	/**
+	 * 
+	 */	
+	
+	/**
+	 * 
+	 */	
+	
+	/**
+	 * 
+	 */	
+	
 }
